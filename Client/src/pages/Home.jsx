8c7b9c6c2c53cbaf0ad235d66bb89940eda18cbd
Himaansh_uu <himan0411singh@gmail.com>
2025-01-26 22:58:03 +0530

    added Shadcn

--- Client/src/pages/Home.jsx
+++ Client/src/pages/Home.jsx
@@ -1,7 +1,10 @@
+import {Button} from "../components/ui/button";
+
 export default function Home() {
     return (
         <div>
             <h1 className="text-red-500">Home</h1>
+            <Button variant="secondary">Login</Button>
         </div>
     );
 }
