d16b49d09bf8e14b82b91fafe0097c419d231b20
vansh1293 <aroravansh.com@gmail.com>
2025-01-27 18:23:00 +0530

    updated user model to store about

--- Server/src/controllers/auth.controller.mjs
+++ Server/src/controllers/auth.controller.mjs
@@ -27,7 +27,7 @@ export const signup = async (req, res) => {
         }
         const salt = await bcrypt.genSalt(10);
         const hashedPassword = await bcrypt.hash(password, salt);
-        const newUser = new User({ email, fullName, password: hashedPassword, profileImage });
+        const newUser = new User({ email, fullName, password: hashedPassword, profileImage, about });
         if (newUser) {
             generateToken(newUser._id, res);
             await newUser.save();
@@ -83,14 +83,24 @@ export const logout = (req, res) => {
 };
 export const updateProfile = async (req, res) => {
     try {
-        const { profileImage } = req.body;
+        const { fullName, about, profileImage } = req.body;
         const userId = req.user._id;
-        if (!profileImage) {
-            return res.status(400).json({ message: 'Profile image is required' });
+        if (!profileImage && !fullName && !about) {
+            return res.status(400).json({ message: 'No fields to update' });
         }
-        const result = cloudinary.uploader.upload(profileImage);
-        const updatedUser = await User.findByIdAndUpdate(userId, { profileImage: result.url }, { new: true });
-        res.status(200).json({ message: 'Profile updated successfully', user: updatedUser });
+        if (profileImage) {
+            const result = cloudinary.uploader.upload(profileImage);
+            const updatedUser = await User.findByIdAndUpdate(userId, { profileImage: result.url, fullName, about }, { new: true });
+            res.status(200).json({ message: 'Profile updated successfully', user: updatedUser });
+        }
+        else if (fullName) {
+            const updatedUser = await User.findByIdAndUpdate(userId, { fullName, about }, { new: true });
+            res.status(200).json({ message: 'Profile updated successfully', user: updatedUser });
+        }
+        else if (about) {
+            const updatedUser = await User.findByIdAndUpdate(userId, { about }, { new: true });
+            res.status(200).json({ message: 'Profile updated successfully', user: updatedUser });
+        }
     } catch (error) {
         console.log("Error in updateProfile controller: ", error);
         res.status(500).json({ message: 'Internal Server Error' });
