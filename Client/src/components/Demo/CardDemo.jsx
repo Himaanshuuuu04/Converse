cbf19d63814f867cf0af6bc5534de383e510f3f6
Himaansh_uu <himan0411singh@gmail.com>
2025-02-15 00:04:19 +0530

    competed the userbar

--- Client/src/components/Demo/CardDemo.jsx
+++ Client/src/components/Demo/CardDemo.jsx
@@ -16,7 +16,7 @@ import { getUsers, setSelectedUser } from "@/redux/slice/chatSlice"
 import { useEffect } from "react"
 import defaultUserImage from '../../assets/defaultUserImage.jpeg'
 import { Input } from "../ui/input"
-
+import { ChevronRight } from 'lucide-react';
 export const CardDemo = ({ className, ...props }) => {
   const dispatch = useDispatch();
   const { users, isUserLoading, selectedUser } = useSelector(state => state.chat);
@@ -25,26 +25,28 @@ export const CardDemo = ({ className, ...props }) => {
   }, [dispatch]);
   return (
     <Card className={cn("w-[380px]", className)} {...props}>
-    <CardHeader>
-      <CardTitle className="text-lg font-semibold">
-        Search Users
-      </CardTitle>
-      <CardDescription className="text-xs  text-muted-foreground">
-        You can only search people you follow
-      </CardDescription>
-      <div className="flex items-center space-x-2">
-        <Input
-          placeholder="Aunt May..."
-          className="w-full"
-          aria-label="Search users"
-        />
-        <Button variant="outline" >
-          Search
-        </Button>
-      </div>
-    </CardHeader>
+    <div className="sticky top-0 z-10 bg-black">
+      <CardHeader>
+        <CardTitle className="text-lg font-semibold">
+          Search Users
+        </CardTitle>
+        <CardDescription className="text-xs  text-muted-foreground">
+          You can only search people you follow
+        </CardDescription>
+        <div className="flex items-center space-x-2">
+          <Input
+            placeholder="Aunt May..."
+            className="w-full"
+            aria-label="Search users"
+          />
+          <Button variant="outline" >
+            Search
+          </Button>
+        </div>
+      </CardHeader>
+    </div>
     
-    <CardContent className="grid gap-2 border-collapse border border-x-0 p-2">
+    <CardContent className="grid gap-0  p-0 overflow-y-auto">
     {isUserLoading ? (
           <div className="text-center text-zinc-500 py-4">Loading users...</div>
         ) : users.length === 0 ? (
@@ -55,18 +57,19 @@ export const CardDemo = ({ className, ...props }) => {
               key={user._id}
               onClick={() => dispatch(setSelectedUser(user._id))}
               className={cn(
-                "flex items-center space-x-4 py-2 px-4 w-full hover:bg-base-300 transition-colors hover:bg-white/5",
-                selectedUser?._id === user._id && "bg-base-300 ring-1 ring-base-300"
+                "flex items-center border bg-base border-collapse space-x-4 py-3 px-4 w-full hover:bg-base-300 transition-colors",
+                selectedUser?._id === user._id && "bg-base-200"
               )}
             >
               <Avatar>
                 <AvatarImage src={user.profileImage || defaultUserImage} className="object-cover"/>
                 <AvatarFallback>{user.name}</AvatarFallback>
               </Avatar>
-              <div className="flex flex-col items-start">
+              <div className="flex flex-col items-start flex-1">
                 <span>{user.fullName}</span>
                 <span className="text-xs text-muted-foreground">Online</span>
               </div>
+              <div className="flex items-center justify-end"><ChevronRight className="w-4 h-4 text-white/50"/></div>
             </button>
     
     ))
--- Client/src/pages/Home.jsx
+++ Client/src/pages/Home.jsx
@@ -4,7 +4,7 @@ export default function Home() {
     return (
         <div className="flex h-screen w-full">
             <div className="flex flex-col overflow-y-auto overflow-x-hidden">
-                <div className="mt-10l-5 flex flex-col  mr-10">
+                <div className="mt-10l-5 flex flex-col  ">
                     <CardDemo className={"w-[380px] bg-black p-0 min-h-screen rounded-none "}/>
                 </div>
             </div>
