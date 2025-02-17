39a143876624311e1acf598ee564649eb203d1d0
Himaansh_uu <himan0411singh@gmail.com>
2025-02-17 11:03:54 +0530

    Final Commit

--- Server/src/index.mjs
+++ Server/src/index.mjs
@@ -13,7 +13,7 @@ app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 app.use(cookieParser());
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: process.env.CLIENT_KEY,
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
