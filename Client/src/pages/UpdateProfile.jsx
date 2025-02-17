ae2ec6331c884b7b0b6b4f668108f3ea46e4f5c0
Himaansh_uu <himan0411singh@gmail.com>
2025-02-17 12:21:32 +0530

    remove unused import for SheetSide in UpdateProfile component

--- Client/src/pages/UpdateProfile.jsx
+++ Client/src/pages/UpdateProfile.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { SheetSide } from "../Components/Demo/SheetDemo";
 import { UpdateForm } from "@/Components/Demo/UpdateForm.jsx";
 
 const UpdateProfile = () => {
