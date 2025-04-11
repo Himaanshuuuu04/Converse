e42d67064e4e4753b1d43aab8420bc3109375d43
Himanshu Singh <himan0411singh@gmail.com>
2025-04-12 00:20:40 +0530

    Update index.mjs

--- Server/src/index.mjs
+++ Server/src/index.mjs
@@ -7,7 +7,7 @@ import cors from 'cors'
 import messageRouter from './routes/message.route.mjs'
 import { app, server } from './lib/socket.mjs'
 dotenv.config()
-
+const __dirname = path.resolve();
 const PORT = process.env.PORT
 app.use(express.json());
 app.use(express.json({ limit: '50mb' }));
