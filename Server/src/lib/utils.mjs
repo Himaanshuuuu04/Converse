362263c89972e0750dba440c09e8e5106ae29880
Himanshu Singh <himan0411singh@gmail.com>
2025-04-22 22:02:03 +0530

    fix: update SameSite attribute for JWT cookie to 'None' for cross-site requests

--- Server/src/lib/utils.mjs
+++ Server/src/lib/utils.mjs
@@ -8,7 +8,7 @@ export const generateToken = (id, res) => {
   res.cookie("jwt", token, {
     httpOnly: true,
     secure: process.env.NODE_ENV !== "development",
-    sameSite: "strict",
+    sameSite: "None",
     path: "/", // explicitly define
     maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
   });
