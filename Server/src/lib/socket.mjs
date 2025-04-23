1dedfccdf64b7fd80db6fe2f7d3136dd293cf083
Himaansh_uu <himan0411singh@gmail.com>
2025-04-23 21:23:07 +0530

    fix: streamline CORS configuration and remove unnecessary transport options

--- Server/src/lib/socket.mjs
+++ Server/src/lib/socket.mjs
@@ -9,14 +9,10 @@ const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
     origin: [
-      process.env.FRONTEND_URL ||
-        "http://localhost:5173" ||
-        "http://localhost:4173",
+      process.env.FRONTEND_URL
     ],
-    methods: ["GET", "POST"],
     credentials: true,
   },
-  transports: ["websocket"],
 });
 
 const userSocketMap = {};
